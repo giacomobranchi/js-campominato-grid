3c6b7b88a905e18e1a2fb9afa3b9149d1034b330
giacomobranchi <branchigiacomo28@gmail.com>
2023-09-01 17:22:03 +0200

    add function to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+
+
+
+
+function generateField(domElement, limit) {
+    console.log(this);
+
+    // genera la griglia
+    for (let i = 0; i < limit; i++) {
+
+        const cellElement = document.createElement('div')
+        cellElement.className = 'cell'
+        cellElement.append(i + 1)
+        domElement.append(cellElement)
+
+        console.log(cellElement);
+
+        // aggiungo l'event listener alla cella che ho appena generato
+        cellElement.addEventListener('click', function () {
+            //console.log('ho cliccato il numero:', cellElement);
+            this.classList.toggle('bg-lightblue')
+            console.log('Il numero selezionato è ' + this.innerHTML);
+        })
+
+    }
+}
+const startButton = document.querySelector('button');
+const fieldElement = document.querySelector('.d-none');
+const limit = 100;
+
+startButton.addEventListener('click', function () {
+    fieldElement.classList.toggle('filed');
+    generateField(fieldElement, limit);
+})
+
+
+
